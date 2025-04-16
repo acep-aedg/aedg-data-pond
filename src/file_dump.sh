b8a7558f578858862ccd8172e03584ffe10ced8b
ianalexmac <iamacdougall@alaska.edu>
2025-04-16 11:08:32 -0400

    add lookup

--- src/file_dump.sh
+++ src/file_dump.sh
@@ -52,6 +52,7 @@ export_to_csv "final" "communities_school_districts" "communities_school_distric
 export_to_csv "intermediate" "lookup_rca_electric_certificates" "lookup_rca_electric_certificates" 
 export_to_csv "intermediate" "lookup_eia_plants_grids" "lookup_eia_plants_grids" 
 export_to_csv "intermediate" "lookup_fips_codes_grids" "lookup_fips_codes_grids" 
+export_to_csv "intermediate" "lookup_fips_codes_sales_reporting_ids" "lookup_fips_codes_sales_reporting_ids"
 
 # Export de-normalized views for public consumption (note: views are special, need the SELECT * query nested in the COPY command)
 psql -h "$PG_HOST" -p "$PG_PORT" -U "$PG_USER" -d "$PG_DB" -c "\COPY (SELECT * FROM public_communities_monthly_generation) TO ./data/public/public_communities_monthly_generation.csv DELIMITER ',' CSV HEADER;"
