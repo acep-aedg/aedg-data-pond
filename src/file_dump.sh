a9f10f326886de8af5dc78023a048f0ade98a12e
ianalexmac <iamacdougall@alaska.edu>
2025-05-20 15:04:57 -0800

    test aggregated cells in csv

--- src/file_dump.sh
+++ src/file_dump.sh
@@ -67,3 +67,4 @@ export_to_csv "public" "public_taxes" "(SELECT * FROM public_taxes)"
 export_to_csv "public" "public_transportation" "(SELECT * FROM public_transportation)"
 export_to_csv "public" "public_yearly_generation" "(SELECT * FROM public_yearly_generation)"
 
+export_to_csv "intermediate" "communities_multiple_election_districts" "(SELECT * FROM communities_multiple_election_districts)"
