0a3a11dbaefbe1b1a9de496dcc55c1ec07edcd79
ianalexmac <iamacdougall@alaska.edu>
2026-08-20 16:41:44 -0800

    enforce ASCII sorting

--- src/alphabetize_data_dictionary.sh
+++ src/alphabetize_data_dictionary.sh
@@ -5,6 +5,6 @@
 DATA_DICTIONARY=registry/fields.csv
 
 # First part pulls the header, second part pulls the rest, third part sorts by first column (-k1), which is 'name'
-(head -n 1 $DATA_DICTIONARY && tail -n +2 $DATA_DICTIONARY | sort -t, -k1) > tmp.csv
+(head -n 1 $DATA_DICTIONARY && tail -n +2 $DATA_DICTIONARY | LC_ALL=C sort -t, -k1) > tmp.csv
 # For safety (otherwise input file will get deleted), output results to tmp.csv then immediately rename to data_dictionary input name
 mv tmp.csv $DATA_DICTIONARY
